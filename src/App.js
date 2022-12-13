df71a29a1470a14497f0748569892043751703fe
Lucas Santis <lucas.santis2@hotmail.com>
2022-12-13 09:58:49 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { Signup } from "./pages/SingUp";
+import { AuthContextComponent } from "./contexts/authContext";
+import { Profile } from "./pages/Profile";
+import { ErrorPage } from "./pages/ErrorPage";
+
+import { ProtectedRoute } from "./components/ProtectedRoute";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AuthContextComponent>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/profile"
+            element={<ProtectedRoute component={Profile} />}
+          />
+
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </AuthContextComponent>
+    </>
   );
 }
 
